c4bf462df27f5b3874a7edd232a53cf8a8ace85c
dinhan1192 <dinhan1192@gmail.com>
2020-02-27 15:07:53 +0700

    dinhan

--- Project_MVC/Controllers/AppRolesController.cs
+++ Project_MVC/Controllers/AppRolesController.cs
@@ -32,7 +32,7 @@ namespace Project_MVC.Controllers
 
         public ActionResult Index()
         {
-            return View(_db.Roles.ToList());
+            return View(_db.IdentityRoles.ToList());
         }
 
         public ActionResult Create()
@@ -43,7 +43,8 @@ namespace Project_MVC.Controllers
         //[Authorize(Roles = "Admin")]
         //[Authorize]
         [HttpPost]
-        public ActionResult Create([Bind(Include = "Name")] AppRole role)
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Name,Description")] AppRole role)
         {
             role.CreatedAt = DateTime.Now;
             if (!roleManager.RoleExists(role.Name))
